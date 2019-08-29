daf0dce4821873f39f302dc75deea0ce4245007a
Ali Khatem <sales@zippax.com>
2019-08-29 13:02:10 +0300

    initial

---
+++ register.php
@@ -0,0 +1,10 @@
+				<?php
+        
+        if($city == 7 || $city == 8){
+					// if city = saihat or qatif ONLY
+					// update default city in home page
+					$cityNameSql = mysql_query("SELECT * FROM cities WHERE id = $city ");
+					$getCity = mysql_fetch_object($cityNameSql);
+					$cityName = $getCity->name;
+					setcookie('maeda_city', $cityName, time() + (31104000 * 30), '/');
+				}
